52cff2d7637e0977be859ad5744113f1c8985369
TahaSavas <taha.savas@hotmail.com>
2021-01-29 10:11:35 +0100

    New Commit

---
+++ LinqConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            d.Task7();
+            d.Task8();
+            d.Task9();
+            d.Task10();
+            d.Task11();
+            d.Task12();
+        }
+    }
+}
